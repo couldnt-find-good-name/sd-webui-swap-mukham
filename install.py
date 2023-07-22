ec3b950ecbae2f653e1133ac11e7643103bf8241
rauldlnx10 <5837219+rauldlnx10@users.noreply.github.com>
2023-07-22 15:34:19 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,20 +1,10 @@
 import launch
 import os
-import pkg_resources
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
-with open(req_file) as file:
-    for package in file:
-        try:
-            package = package.strip()
-            if '==' in package:
-                package_name, package_version = package.split('==')
-                installed_version = pkg_resources.get_distribution(package_name).version
-                if installed_version != package_version:
-                    launch.run_pip(f"install {package}", f"swap mukham requirement: changing {package_name} version from {installed_version} to {package_version}")
-            elif not launch.is_installed(package):
-                launch.run_pip(f"install {package}", f"swap mukham requirement: {package}")
-        except Exception as e:
-            print(e)
-            print(f'Warning: Failed to install {package}.')
+try:
+    launch.run_pip(f"install -r {req_file}", f"sd-webui-swap-mukham requirement: installing dependencies from {req_file}")
+except Exception as e:
+    print(e)
+    print(f'Warning: Failed to install dependencies from {req_file}.')
