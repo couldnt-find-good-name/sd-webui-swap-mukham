10c0be73405d99d1399055790ee0c861e9cea5cf
rauldlnx10 <5837219+rauldlnx10@users.noreply.github.com>
2023-07-22 05:11:12 +0200

    Update face_enhancer.py

--- face_enhancer.py
+++ face_enhancer.py
@@ -14,14 +14,14 @@ def realesrgan_runner(img, model):
     img = model.predict(img)
     return img
 
-
 supported_enhancers = {
     "GFPGAN": ("./assets/pretrained_models/GFPGANv1.4.pth", gfpgan_runner),
+    "GFPGANx2": ("./assets/pretrained_models/GFPGANv1.4.pth", gfpgan_runner),
     "REAL-ESRGAN 2x": ("./assets/pretrained_models/RealESRGAN_x2.pth", realesrgan_runner),
     "REAL-ESRGAN 4x": ("./assets/pretrained_models/RealESRGAN_x4.pth", realesrgan_runner),
-    "REAL-ESRGAN 8x": ("./assets/pretrained_models/RealESRGAN_x8.pth", realesrgan_runner)
+    "REAL-ESRGAN 8x": ("./assets/pretrained_models/RealESRGAN_x8.pth", realesrgan_runner),
+    "4x_RealisticRescaler_100000_G": ("./assets/pretrained_models/4x_RealisticRescaler_100000_G.pth", realesrgan_runner),
 }
-
 
 def get_available_enhancer_names():
     available = []
@@ -31,13 +31,14 @@ def get_available_enhancer_names():
             available.append(name)
     return available
 
-
 def load_face_enhancer_model(name='GFPGAN', device="cpu"):
     assert name in get_available_enhancer_names(), f"Face enhancer {name} unavailable."
     model_path, model_runner = supported_enhancers.get(name)
     model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), model_path)
     if name == 'GFPGAN':
         model = gfpgan.GFPGANer(model_path=model_path, upscale=1, device=device)
+    elif name == 'GFPGANx2':
+        model = gfpgan.GFPGANer(model_path=model_path, upscale=2, device=device)
     elif name == 'REAL-ESRGAN 2x':
         model = RealESRGAN(device, scale=2)
         model.load_weights(model_path, download=False)
@@ -47,6 +48,10 @@ def load_face_enhancer_model(name='GFPGAN', device="cpu"):
     elif name == 'REAL-ESRGAN 8x':
         model = RealESRGAN(device, scale=8)
         model.load_weights(model_path, download=False)
+    elif name == '4x_RealisticRescaler_100000_G':
+        model = RealESRGAN(device, scale=4)
+        model.load_weights(model_path, download=False)           
     else:
         model = None
     return (model, model_runner)
+
