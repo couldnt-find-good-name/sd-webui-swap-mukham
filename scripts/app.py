aed9252242f1a21bcc06764228308a73520856a1
rauldlnx10 <5837219+rauldlnx10@users.noreply.github.com>
2023-08-05 14:28:41 +0200

    Update app.py

--- scripts/app.py
+++ scripts/app.py
@@ -12,7 +12,7 @@ import insightface
 import onnxruntime
 import numpy as np
 import gradio as gr
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import concurrent.futures
 from moviepy.editor import VideoFileClip
 import requests
