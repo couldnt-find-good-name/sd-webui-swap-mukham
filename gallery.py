8c56f141ec8a7140e1b13c65fdcf21f6c9db82c1
rauldlnx10 <5837219+rauldlnx10@users.noreply.github.com>
2023-08-12 10:50:49 +0200

    Update gallery.py

--- gallery.py
+++ gallery.py
@@ -57,6 +57,54 @@ def gallery_others():
     yield gr.Gallery.update(value=others_img_list)  # Devolver el nuevo valor de la galería
 
 
+
+    #############################  VIDEO GALLERY ###################################################
+#
+videogallery = """current_dir = outputs_dir
+            video_list = []     # Lista para guardar los videos
+
+            def video_gallery(current_dir):
+                video_list = []
+                for root, dirs, files in os.walk(current_dir):
+                    for file in files:
+                        if file.endswith(".mp4"):
+                            video_path = os.path.join(root, file)
+                            video_list.append(video_path)
+                return video_list
+
+            def update_video_gallery(sender, data):
+                all_videos = video_gallery(current_dir)
+                print(all_videos)
+                # Usamos un bucle for para actualizar cada componente gr.Video con el valor correspondiente de la lista de videos
+                for i in range(3):
+                    video_gal[i].update(value=all_videos[i])
+
+            def get_num_rows(video_list, videos_per_row):
+                # Esta función calcula el número de filas necesarias para mostrar los videos
+                num_videos = len(video_list)
+                num_rows = num_videos // videos_per_row
+                if num_videos % videos_per_row != 0:
+                    num_rows += 1
+                return num_rows
+
+            with gr.Tab("Videos", elem_id="videos_tab"):
+                update_button = gr.Button(value="Update Gallery", variant="primary")
+                all_videos = video_gallery(current_dir)
+                videos_per_row = 4
+                num_rows = get_num_rows(all_videos, videos_per_row)
+
+                for i in range(num_rows):
+                    with gr.Row():
+                        # Usamos una lista por comprensión para crear los objetos gr.Video a partir de la lista de videos
+                        video_gal = [gr.Video(all_videos[i * videos_per_row + j]) for j in range(videos_per_row) if i * videos_per_row + j < len(all_videos)]
+
+                update_button.click(fn=update_video_gallery, outputs=video_gal)
+                preview_video.change(fn=update_video_gallery, outputs=video_gal)
+"""
+#############################  VIDEO GALLERY ###################################################  
+
+
+
 def update_galleries():
     # Crear tres variables vacías para guardar las imágenes de cada tab
     txt2img_img_list = []
